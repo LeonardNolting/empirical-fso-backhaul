e864f170d72b009c4059da419d4fa8fca9b1e187
AsterixxxGallier <asterixxxgallier@aol.com>
2026-08-20 22:20:45 +0200

    specify unit for --max-link-length argument

--- los-checker/src/cli.rs
+++ los-checker/src/cli.rs
@@ -2,7 +2,7 @@ use clap::Parser;
 
 #[derive(Parser, Debug)]
 pub struct Args {
-    /// Maximum link length
+    /// Maximum link length in meters
     #[arg(short, long, default_value_t = f64::INFINITY)]
     pub max_link_length: f64,
     #[arg(short, long, default_value = "paris_small")]
